1ddda884d7f3b9ffaec1585a1118e00c7a99d8a3
DSlundro <dragan.savic95@outlook.it>
2022-05-16 17:39:27 +0200

    add top learners

--- src/components/partials/main/TopLearners.vue
+++ src/components/partials/main/TopLearners.vue
@@ -14,9 +14,47 @@
             </div>
         </div>
 
-        <div class="row m-0">
-            <div class="col-4">
-                
+        <div class="sc">
+            <div class="row justify-content-around">
+
+                <!-- CARD -->
+                <div class="tlb-card col-4 d-flex flex-column px-5" v-for="(learner, index) in learners" :key="index">
+                    <!-- TITLE -->
+                    <div class="tlb-title py-2">
+                        <h6>{{learner.title}}</h6>
+                    </div>
+                    <!-- TEXT -->
+                    <div class="tlb-text py-2">
+                        <p>{{learner.text}}</p>
+                    </div>
+                    <!-- USER -->
+                    <div class="tlb-user d-flex align-items-center gap-4 py-3">
+                        <!-- IMG -->
+                        <div class="tlb-user-img">
+                            <img :src="learner.img" alt="">
+                        </div>
+                        <!-- DESCRIPTION -->
+                        <div class="tlb-user-description d-flex flex-column justify-content-center gap-3">
+                            <!-- NAME -->
+                            <div class="tlb-user-name">
+                                <h6 class="m-0">{{learner.name}}</h6>
+                            </div>
+                            <!-- JOB -->
+                            <div class="tlb-user-job">
+                                <p class="m-0">/ {{learner.job}}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 text-center py-5">
+                    <div class="d-flex gap-2 justify-content-center">
+                        <font-awesome-icon icon="fa-solid fa-circle" />
+                        <font-awesome-icon icon="fa-solid fa-circle" />
+                        <font-awesome-icon icon="fa-solid fa-circle" />
+                        <font-awesome-icon icon="fa-solid fa-circle" />
+                    </div>
+                </div>
             </div>
         </div>
     </section>
@@ -25,6 +63,33 @@
 <script>
 export default {
     name: 'TopLearners',
+    data(){
+        return{
+            learners:[
+                {
+                    img: require('@/assets/img/ta-2.jpg'),
+                    name: 'Florence Themes',
+                    job: 'Multimedia Admin',
+                    title: `It's a choice of quality for people with special needs`,
+                    text: `I'm a very scrict person so I require everything to be organized and neat. Then, I'll be able to make things right and shine. MaxCoach guys just got me.`,
+                },
+                {
+                    img: require('@/assets/img/ta-3.jpg'),
+                    name: 'Mina Hollace',
+                    job: 'Freelancer',
+                    title: `High level of efficiency and scientific teaching methond`,
+                    text: `I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.`,
+                },
+                {
+                    img: require('@/assets/img/ta-4.jpg'),
+                    name: 'Madley Pondor',
+                    job: 'IT Specialist',
+                    title: `Professional team of specialist and passionate mentors at reach`,
+                    text: `I need to get a certification for English proficiency and MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.`,
+                },
+            ]
+        }
+    }
 }
 </script>
 
@@ -33,4 +98,11 @@ export default {
         h6{color: $Shady-Lady;text-transform: uppercase;}
         span{color: $Turquoise-Blue;font-weight: 100;}
     }
+.tlb-card{
+    .tlb-title h6{color: $Cod-Gray;}
+    .tlb-text p{color: $Shady-Lady;}
+    .tlb-user-img img{width: 80px; border-radius: 50%;}
+    .tlb-user-name h6{text-transform: uppercase;}
+    .tlb-user-job p{color: $Shady-Lady;}
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 
 /* import specific icons */
 import { faCircleUser, faFileLines, faUser,} from '@fortawesome/free-regular-svg-icons'
-import { faBook, faCartShopping, faLifeRing,  faDownload, faMagnifyingGlass, faO, faHeadset, faRightLong } from '@fortawesome/free-solid-svg-icons'
+import { faBook, faCartShopping, faLifeRing,  faDownload, faMagnifyingGlass, faO, faHeadset, faRightLong, faCircle } from '@fortawesome/free-solid-svg-icons'
 
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -21,7 +21,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* add icons to the library */
 library.add(
   faCircleUser, faMagnifyingGlass, faDownload, faO, 
-  faCartShopping, faBook, faLifeRing, faHeadset, faRightLong, faFileLines, faUser)
+  faCartShopping, faBook, faLifeRing, faHeadset, faRightLong, faFileLines, faUser, faCircle)
 
 /* add font awesome icon component */
 Vue.component('font-awesome-icon', FontAwesomeIcon)
